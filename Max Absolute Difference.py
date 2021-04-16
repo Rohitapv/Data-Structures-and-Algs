98e49bbb050a3148eb80a1970aff879cca13dab4
bharat-chandra <53616992+bharat-chandra@users.noreply.github.com>
2021-04-16 22:31:56 +0530

    Update and rename Max Absolute Difference.c to Max Absolute Difference.py

--- Max Absolute Difference.py
+++ Max Absolute Difference.py
@@ -15,21 +15,31 @@ Sample Input 0
 Sample Output 0
 5
   
-  
-  
-def maxDistance(array):
-	max1 = -2147483648
-	min1 = +2147483647
-	max2 = -2147483648
-	min2 = +2147483647
-	for i in range(len(array)):
-		max1 = max(max1, array[i] + i)
-		min1 = min(min1, array[i] + i)
-		max2 = max(max2, array[i] - i)
-		min2 = min(min2, array[i] - i)
-	return max(max1 - min1, max2 - min2)
+#O(n^2)
+"""n=int(input())
+l=list(map(int,input().split()))
+res=0
+for i in range(n):
+    for j in range(i,n):
+        val = abs(l[i]-l[j])+abs(i-j)
+        if val > res:
+            res = val
 
-array = [ -70, -64, -6, -56, 64,
-		61, -57, 16, 48, -98 ]
+print(res)"""
+#O(n)
+def maxDistance(array):
+    max1 = -2147483648
+    min1 = +2147483647
+    max2 = -2147483648
+    min2 = +2147483647
+
+    for i in range(len(array)):
+        max1 = max(max1, array[i] + i)
+        min1 = min(min1, array[i] + i)
+        max2 = max(max2, array[i] - i)
+        min2 = min(min2, array[i] - i)
+    return max(max1 - min1, max2 - min2)
+n=int(input())
+array = list(map(int,input().split()))
 
 print(maxDistance(array))
